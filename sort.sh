027c65b37f9a3f8687a350dc886b7187072db575
alam0rt <sam@samlockart.com>
2025-02-21 10:45:35 +1100

    sorted

--- sort.sh
+++ sort.sh
@@ -6,5 +6,5 @@ GEN_DIR="$DIR/generated"
 
 mkdir -p "$GEN_DIR"
 
-cat "$SOURCE" | awk '/^[a-z]/' | sort  | grep FUN > "$GEN_DIR/unsorted_functions.txt"
-cat "$SOURCE" | awk '/^[a-z]/' | sort  > "$GEN_DIR/unsorted_all.txt"
+cat "$SOURCE" | grep -E '^[a-zA-Z_][a-zA-Z0-9_]*[[:space:]]+[a-zA-Z_][a-zA-Z0-9_]*\(.*\)' > "$GEN_DIR/functions.txt"
+cat "$SOURCE" | grep -E '^[[:space:]]*(typedef|struct|enum|union)' | sort  > "$GEN_DIR/types.txt"
